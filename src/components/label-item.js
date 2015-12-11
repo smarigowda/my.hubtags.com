91d29b7f37cffc9a3acfa69b9baa888c7e0f57f6
Marigowda, Santosh <santosharakere@gmail.com>
2015-12-11 04:51:22 +0000

    Fri 11 Dec 2015 04:51:22 GMT

--- src/components/label-item.js
+++ src/components/label-item.js
@@ -3,6 +3,36 @@ import React from 'react'
 export default React.createClass({
 	render() {
 		const { label } = this.props
-		return <li><h5>{label.name}</h5></li>
+		// return <li><h5>{label.name}</h5></li>
+
+		let content;
+
+		// editing
+		if() {
+			content = (
+				<form className='label'>
+				  <span className='label-color avatar avatar-small avatar-rounded'>&nbsp;</span>
+				  <input name='name'/>
+				  <input name='color'/>
+				  <button type='submit' className='button button-small'>Save</button>
+				  <button type='button' className='button button-small button-unstyled'>cancel</button>
+				</form>
+			)
+		} else {
+			content = (
+				<div className='label'>
+				  <span className='label-color' style={{backgroundColor: cssColor}}>&nbsp;</span>
+				  <span></span>
+				  <span className='octicon octicon-pencil'></span>
+				  <span className='octicon octicon-x'></span>
+				</div>
+			)
+		}
+
+		return (
+			<div>
+				{content}
+			</div>
+		)
 	}
 })
